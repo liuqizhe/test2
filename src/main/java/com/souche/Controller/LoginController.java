932ca48c302c4057e9ff479fecd70c23d8f27a89
liuqizhe <lqz0618@aliyun.com>
2015-09-22 10:29:38 +0800

    modify test2

--- src/main/java/com/souche/Controller/LoginController.java
+++ src/main/java/com/souche/Controller/LoginController.java
@@ -1,6 +1,8 @@
 package com.souche.Controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.ModelAndView;
@@ -18,6 +20,24 @@ public class LoginController{
     //该方法对应/login/tologin.from
     @RequestMapping("/toLogin")
     public String toLogin() {
+        System.out.println("run toLogin");
         return "login" ;
+    }
+
+    @RequestMapping("login3")
+    public String checkLogin3(User user, ModelMap map) {
+        //检查用户名和密码，假设用户名scott，密码123456
+        System.out.println("run checkLogin3");
+        if("scott".equals(user.getUsername()) && "123456".equals(user.getPassword())) {
+            map.addAttribute("user", user.getUsername()) ;
+            return "ok" ;
+        } else {
+            return "login" ;
+        }
+    }
+
+    @ModelAttribute("name")
+    public User getUser() {//早于SessionAttribute
+        return new User() ;
     }
 }
--- target/com.souche.test/WEB-INF/jsp/login.jsp
+++ target/com.souche.test/WEB-INF/jsp/login.jsp
@@ -3,7 +3,7 @@
 	<head></head>
 	<body>
 		<!-- '/'表示http://localhost:8088/ -->
-		 <form action="/SpringMvc02/login/login3.from" method="post">
+		 <form action="/test2/login/login3.from" method="post">
 			用户名：<input type="text" name="username"/><br/>
 			密码：<input type="password" name="password"/><br/>
 			<input type="submit" value="提交"/>
